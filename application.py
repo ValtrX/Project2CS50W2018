601a50cb3cb2bb7ac586388feec12df83e90a6ff
Carlos Bermudez <grateroldiaz@hotmail.com>
2020-05-26 19:24:52 -0400

    First update without design

--- application.py
+++ application.py
@@ -4,20 +4,45 @@ from dotenv import load_dotenv
 load_dotenv('.flaskenv')
 
 from flask import Flask, render_template, request
-from flask_socketio import SocketIO, send, emit
+from flask_socketio import SocketIO, send, emit, join_room, leave_room
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+ROOMS = ["room1", "room2 ", "room3"]
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", rooms=ROOMS)
 
 @socketio.on('message') #When server is listening to an event called message
-def handle_message(msg):
+def handle_message(data):
 
-    send(msg, broadcast = True)
+    msg = data["msg"]
+    room = data["room"]
+    print(f"{data}")
+    send({"msg": msg}, room=room)
+
+@socketio.on('join')
+def join(data):
+    room = data["room"]
+    join_room(room)
+    send({"msg": "someone has joined to: " + room}, room=room, broadcast = True)
+
+@socketio.on('leave')
+def leave(data):
+    room = data["room"]
+    leave_room(room)
+    send({"msg": "someone has left"}, room=room)
+
+@socketio.on('new_room')
+def new_room(data):
+    ROOMS.append(data["new_room_name"])
+    room = data["new_room_name"]
+    join_room(data['new_room_name'])
+    # Notification about new chat room created
+    send({"msg": "someone has created the " + room + " room."}, room=room, broadcast=True)
 
 
 if __name__ == '__main__':
--- static/index.js
+++ static/index.js
@@ -4,24 +4,120 @@ document.addEventListener('DOMContentLoaded', () => {
     // Connect to websocket
     const socket = io();
 
-    socket.on('message', msg => {
+    // Default room
+    let room = "room1"
+    joinRoom("room1");
 
+    // Get username and save it on localstorage
+    
+    var userDisplay = document.querySelector("#show_username").textContent
+
+    //document.querySelector("#set-username").onclick=function(){
+    //    myUsername = document.querySelector("#myUsername").value
+    //    localStorage.setItem('username', myUsername);
+    //    var username = localStorage.getItem('username');
+    //    document.querySelector("#show_username").append(username);
+    //};
+    //var username = localStorage.getItem('username');
+    //document.querySelector("#show_username").append(username);
+    //console.log(username);
+
+    var username = localStorage.getItem('username');
+    document.querySelector("#show_username").classList.add('d-none');
+
+    if (username == null){
+        document.querySelector("#set-username").onclick=function(){
+            myUsername = document.querySelector("#myUsername").value
+            localStorage.setItem('username', myUsername);
+            var username = localStorage.getItem('username');
+            document.querySelector("#show_username").append(username);
+            document.querySelector("#show_username").classList.remove('d-none');
+            document.querySelector("#user_display").classList.add('d-none');
+        
+        };
+    }
+    else{
+        var username = localStorage.getItem('username');
+        document.querySelector("#show_username").append(username);
+        document.querySelector("#show_username").classList.remove('d-none');
+        document.querySelector("#user_display").classList.add('d-none');
+    }
+    
+
+    // Display incoming messages
+    socket.on('message', data => {
 
         const li = document.createElement('li');
-        li.innerHTML = `message: ${msg}`;
-        document.getElementById("messages").append(li);
-
+        li.innerHTML = `${data.msg}`;
+        document.querySelector("#messages").append(li);
     });
 
-    
-
-    document.getElementById("send").onclick=function(){
-        
-       
-        socket.send(document.getElementById("myMessage").value);
-
-        document.getElementById("myMessage").value = '';
+    // Sending a message
+    document.querySelector("#send").onclick=function(){
+        socket.emit('message', {'msg': document.querySelector("#myMessage").value, 'room': room});
+        document.querySelector("#myMessage").value = '';
     };
     
+    // Room selection
+    document.querySelectorAll('.select-room').forEach(li => {
+        li.onclick = () => {
+            let newRoom = li.innerHTML;
+
+            if (newRoom == room){
+                msg = `You are already in ${room}`;
+                printSysMsg(msg);
+            }else{
+                leaveRoom(room);
+                joinRoom(newRoom);
+                room = newRoom;
+            }
+             
+        };
+    });
+
+    // Leave room
+    function leaveRoom(room){
+        socket.emit('leave', {'room': room});
+    }
+
+    // Join room
+    function joinRoom(room){
+        socket.emit('join', {'room': room});
+        document.querySelector('#messages').innerHTML = ''
+    }
+
+    // Get data from the setnewRoom input id
+
+    document.querySelector("#set_newRoom").onclick=function(){
+        const rName = document.querySelector("#setnewRoom").value.trim();
+        if(rName === ""){
+            alert('channel name is empty');
+        }
+        else{
+            socket.emit('new_room', {'new_room_name': rName});
+
+            // add new room to the room <li>
+           
+            const li = document.createElement('li');
+            li.setAttribute('class','select-room');
+            //console.log(li.innerHTML = rName);
+             
+            document.querySelector("#rooms").append(li);
+        }
+        
+        
+    };
+
     
+
+
+
+    // Print system message
+    function printSysMsg(msg){
+      
+        const li = document.createElement('li');
+        li.innerHTML = msg;
+        document.querySelector("#messages").append(li);
+    }
+
 });
